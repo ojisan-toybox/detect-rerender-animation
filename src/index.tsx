22b77583b34b7b0e94763332a5e19ee27394b0af
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-09 05:04:04 +0900

    cnt

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,37 @@
 import { h, render } from "preact";
+import { StateUpdater, useState } from "preact/compat";
+import { setup, styled } from "goober";
 
-const Main = () => {
-  return <div>helo</div>;
+setup(h);
+
+const Component = (props: { cnt: number; setCnt: StateUpdater<number> }) => {
+  return (
+    <div>
+      <span>cnt: {props.cnt}</span>
+      <button onClick={() => props.setCnt(props.cnt + 1)}>count up</button>
+    </div>
+  );
 };
 
-render(<Main></Main>, document.body);
+const StyledComponent = styled(Component)`
+  @keyframes rendered {
+    0% {
+      background-color: rgb(255, 255, 0, 1);
+    }
+    100% {
+      background-color: rgb(255, 255, 0, 0);
+    }
+  }
+
+  span {
+    animation: rendered 1s ease;
+    animation-iteration-count: 1;
+  }
+`;
+
+const ContainerComponent = () => {
+  const [cnt, setCnt] = useState(0);
+  return <StyledComponent {...{ cnt, setCnt }}></StyledComponent>;
+};
+
+render(<ContainerComponent></ContainerComponent>, document.body);
